d3a321d3e7b66e7cb0ce48356599b39ffb117db7
Serenity-Sun <DreamTravel1227@gmail.com>
2021-01-25 00:28:27 +0600

    новая команда generate [имя мира] [тип генерации]

--- src/Kimi/Manager.php
+++ src/Kimi/Manager.php
@@ -3,6 +3,10 @@
 namespace Kimi;
 
 
+use Kimi\generator\Nothing;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\level\generator\GeneratorManager;
 use pocketmine\plugin\PluginBase;
 
 class Manager extends PluginBase
@@ -11,6 +15,18 @@ class Manager extends PluginBase
 
     public function onEnable()
     {
+        GeneratorManager::addGenerator(Nothing::class, "nothing");
 
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
+    {
+        switch ($command->getName())
+        {
+            case "generate":
+                break;
+        }
+
+        return true;
     }
 }
